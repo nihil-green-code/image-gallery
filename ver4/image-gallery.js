e14ed65609123e5cb59ce8a15178eac7917376af
NIHILncunia <nihil_ncunia@naver.com>
2022-10-17 20:25:32 +0900

    2022 1017 모듈이 통과할까?

--- ver4/image-gallery.js
+++ ver4/image-gallery.js
@@ -21,33 +21,38 @@ for (let i = 0; i < images.length; i++) {
 
 // ==================== 슬라이드  ====================
 
-// 1. slideCount 없애고 다른 방법을 강구하라.
-// 2. setInterval이 두번 나온다.
+// 1. slideCount 없애고 다른 방법을 강구하라. = 해결
+// 2. setInterval이 두번 나온다. 모듈을 이용하라. = 심사 대기
 
 const $nationBox = $container.getElementsByClassName('nationBox');
 let tmp = $nationBox[0];
-let intervalId, slideCount = 1, trigger = true;
+let intervalId, trigger = true;
 
 function slide() {
-  return setInterval(function () {
-    $img.setAttribute('src', images[slideCount]);
+  const computedIndex = tmp.textContent % images.length;
 
-    tmp.style.opacity = '.3';
-    $nationBox[slideCount].style.opacity = '1';
-    tmp = $nationBox[slideCount++];
-
-    if (slideCount === images.length) slideCount = 0;
-  }, 1500);
+  $img.setAttribute('src', images[computedIndex]);
+  tmp.style.opacity = '.3';
+  $nationBox[computedIndex].style.opacity = '1';
+  tmp = $nationBox[computedIndex];
 }
 
-intervalId = slide();
+function start() {
+  intervalId = setInterval(slide, 1500);
+};
+
+function stop() {
+  clearInterval(intervalId);
+};
+
+start();
 
 $container.addEventListener('click', function () {
   if (trigger) {
-    clearInterval(intervalId);
+    stop()
   }
   else {
-    intervalId = slide();
+    start();
   }
 
   trigger = !trigger;
